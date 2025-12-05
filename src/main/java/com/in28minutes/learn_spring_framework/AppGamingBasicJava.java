f56c50ff749ff1760f1eab7abd5dd9e3fc8e6d45
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2025-12-05 22:23:50 +0530

    Add PacmanGame to prove lose coupling

--- src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
+++ src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
@@ -1,13 +1,14 @@
 package com.in28minutes.learn_spring_framework;
-
 import com.in28minutes.learn_spring_framework.game.GameRunner;
-import com.in28minutes.learn_spring_framework.game.MarioGame;
-import com.in28minutes.learn_spring_framework.game.SuperContraGame;
+//import com.in28minutes.learn_spring_framework.game.MarioGame;
+import com.in28minutes.learn_spring_framework.game.PacmanGame;
+//import com.in28minutes.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args){
 //        MarioGame game = new MarioGame();
-        SuperContraGame game = new SuperContraGame();
+//        SuperContraGame game = new SuperContraGame();
+        PacmanGame game = new PacmanGame();
         GameRunner gameRunner = new GameRunner(game);
         gameRunner.run();
     }
